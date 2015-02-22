766a50d87b118c9fe7b5cb0e1ccf891d847ee3eb
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2015-02-23 03:31:49 +1100

    Create fabric for RadioDevice hw abstraction (WIP)
    Provide NullDevice as a stub device of what would be real hardware.
    
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/BTS/RadioDrivers/NullDevice.hs
@@ -0,0 +1,198 @@
+{-|
+  Module      : $Header$
+  Copyright   : (c) 2014 Edward O'Callaghan
+  License     : GPL-2
+  Maintainer  : eocallaghan@alterapraxis.com
+  Stability   : provisional
+  Portability : portable
+
+  Diagnostic soft-hardware.
+-}
+
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module BTS.RadioDrivers.NullDevice where
+
+import Control.Applicative (Applicative(..), (<$>))
+import Control.Monad (ap)
+import Control.Monad.Trans
+import Control.Monad.Trans.State
+import Control.Monad.IO.Class
+import Data.Time.Clock
+
+import BTS.RadioDevice
+
+
+data HwState = HwState { sampleRate     :: Double    -- ^ the desired sampling rate
+                       , samplesRead    :: Integer   -- ^ number of samples read from SDR
+                       , samplesWritten :: Integer   -- ^ number of samples sent to SDR
+                       , currentStamp   :: TimeStamp -- ^ internal timestamp clock state
+                       , startTime      :: UTCTime   -- ^ ..
+                       , underRun       :: Bool      -- ^ ..
+                       }
+
+newtype NullDevice a = NullDevice { unNullDevice :: StateT HwState IO a }
+                     deriving (Monad, MonadIO)
+
+instance Functor NullDevice where
+  {-# INLINE fmap #-}
+  fmap f m = NullDevice (f <$> unNullDevice m)
+
+instance Applicative NullDevice where
+  {-# INLINE pure #-}
+  pure  = return
+  {-# INLINE (<*>) #-}
+  (<*>) = ap
+
+--  short *dummyBurst;
+--  int dummyBurstSz;
+--  int dummyBurstCursor;
+--  bool underrun;
+
+
+-- Type'ed state transistion structure
+type Selector a = (NullDevice a, a -> NullDevice ())
+
+s4 :: Selector TimeStamp
+s4 = (NullDevice (gets currentStamp), \x -> NullDevice (modify (\vs -> vs {currentStamp = x})))
+
+s6 :: Selector Bool
+s6 = (NullDevice (gets underRun), \x -> NullDevice (modify (\vs -> vs {underRun = x})))
+
+sel :: Selector a -> NullDevice a
+sel = fst
+
+mods :: Selector a -> (a -> a) -> NullDevice ()
+mods (gf,uf) mfun = do st <- gf
+                       uf (mfun st)
+
+
+updateTime :: NullDevice ()
+updateTime  = do
+  sr <- NullDevice $ gets sampleRate
+  st <- NullDevice $ gets startTime
+  ct <- liftIO getCurrentTime
+  mods s4 (const $ floor (sr * realToFrac (diffUTCTime ct st)))
+
+runNullDevice :: Double -> NullDevice a -> IO a
+runNullDevice sr m = getCurrentTime >>= \curTime -> evalStateT (unNullDevice m) (HwState sr 0 0 0 curTime False)
+
+bracket start stop body = do
+     start
+     body
+     stop
+
+-- | Object constructor
+--  LOG(INFO) << "creating SDR device...";
+withNullDevice :: Double -> NullDevice a -> IO ()
+withNullDevice sr stuff = runNullDevice sr (bracket nullDeviceStart nullDeviceStop stuff)
+
+
+-- | Construct an instance of the RadioDevice interface type with NullDevice
+constructNullDevice :: IO (RadioDevice NullDevice)
+constructNullDevice = do
+  let nulldev = RadioDevice { setVCTCXO             = undefined
+                            , setTxFreq             = nullDeviceSetTxFreq
+                            , setRxFreq             = nullDeviceSetRxFreq
+                            -- **
+                            , setRxGain             = undefined
+                            , setTxGain             = undefined
+                            , getRxGain             = return 0.0
+                            -- **
+                            , getMaxRxGain          = return 0.0
+                            , getMinRxGain          = return 0.0
+                            , getMaxTxGain          = return 0.0
+                            , getMinTxGain          = return 0.0
+                            -- **
+                            , getTxFreq             = nullDeviceGetTxFreq
+                            , getRxFreq             = nullDeviceGetRxFreq
+                            , getSampleRate         = nullDeviceGetSampleRate
+                            , numberRead            = nullDeviceNumberRead
+                            , numberWritten         = nullDeviceNumberWritten
+                            -- **
+                            , updateAlignment       = nullDeviceUpdateAlignment
+                            -- **
+                            , initialWriteTimestamp = nullDeviceInitialWriteTimestamp
+                            , initialReadTimestamp  = nullDeviceInitialReadTimestamp
+                            -- **
+                            , fullScaleInputValue   = nullDeviceFullScaleInputValue
+                            , fullScaleOutputValue  = nullDeviceFullScaleOutputValue
+                            -- **
+                            , readSamples           = nullDeviceReadSamples
+                            , writeSamples          = nullDeviceWriteSamples
+                            }
+
+  return nulldev
+
+
+-- public:
+
+-- int loadBurst(short *wDummyBurst, int len);
+--  dummyBurst = wDummyBurst;
+--  dummyBurstSz = len;
+--  return 0;
+
+-- | Start the SDR
+nullDeviceStart :: NullDevice ()
+nullDeviceStart  = do
+ liftIO $ putStrLn $ "INFO " ++ "starting SDR..."
+
+-- | Stop the SDR
+nullDeviceStop :: NullDevice ()
+nullDeviceStop  = do
+ liftIO $ putStrLn $ "INFO " ++ "stopping SDR..."
+
+-- | ..
+nullDeviceReadSamples :: TimeStamp -> NullDevice ()
+nullDeviceReadSamples ts = undefined -- XXX
+
+-- | ..
+nullDeviceWriteSamples :: TimeStamp -> NullDevice ()
+nullDeviceWriteSamples ts = do
+  updateTime
+  cts <- NullDevice $ gets currentStamp
+  urun <- NullDevice $ gets underRun
+  mods s6 (const $ urun || (cts < ts))
+
+-- | Update the alignment between the read and write timestamps
+nullDeviceUpdateAlignment :: TimeStamp -> NullDevice ()
+nullDeviceUpdateAlignment t = return ()
+
+-- | Set the transmitter frequency
+nullDeviceSetTxFreq :: Double -> Double -> NullDevice ()
+nullDeviceSetTxFreq f a = return ()
+
+-- | Set the receiver frequency
+nullDeviceSetRxFreq :: Double -> Double -> NullDevice ()
+nullDeviceSetRxFreq f a = return ()
+
+-- | Returns the starting write Timestamp
+nullDeviceInitialWriteTimestamp :: NullDevice TimeStamp
+nullDeviceInitialWriteTimestamp = return 20000
+
+-- | Returns the starting read Timestamp
+nullDeviceInitialReadTimestamp :: NullDevice TimeStamp
+nullDeviceInitialReadTimestamp = return 20000
+
+-- | returns the full-scale transmit amplitude
+nullDeviceFullScaleInputValue :: NullDevice Double
+nullDeviceFullScaleInputValue = return 13500.0
+
+-- | returns the full-scale receive amplitude
+nullDeviceFullScaleOutputValue :: NullDevice Double
+nullDeviceFullScaleOutputValue = return 9450.0
+
+-- | Return internal status values
+nullDeviceGetTxFreq :: NullDevice Double
+nullDeviceGetTxFreq = return 0
+
+nullDeviceGetRxFreq :: NullDevice Double
+nullDeviceGetRxFreq = return 0
+
+nullDeviceGetSampleRate :: NullDevice Double
+nullDeviceGetSampleRate = NullDevice $ gets sampleRate
+
+nullDeviceNumberRead :: NullDevice Integer
+nullDeviceNumberRead = NullDevice $ gets samplesRead
+
+nullDeviceNumberWritten :: NullDevice Integer
+nullDeviceNumberWritten = NullDevice $ gets samplesWritten
---
+++ src/BTS/RadioInterface.hs
@@ -0,0 +1,110 @@
+{-|
+  Module      : $Header$
+  Copyright   : (c) 2014 Edward O'Callaghan
+  License     : GPL-2
+  Maintainer  : eocallaghan@alterapraxis.com
+  Stability   : provisional
+  Portability : portable
+
+  This module covers L1 FEC, L2 and L3 message translation.
+-}
+
+module BTS.RadioInterface where
+
+import BTS.GSMCommon
+import BTS.RadioDevice
+
+-- | Organised GSM bursts by GSM timestamps
+data RadioVector = RadioVector { time  :: Time -- ^ the burst's GSM timestamp
+                               , arfcn :: Int
+                               } deriving (Eq)
+
+-- comparison operator, used for sorting
+instance Ord RadioVector where
+    rv <  rv' = time rv <  time rv'
+    rv >  rv' = time rv >  time rv'
+    rv <= rv' = time rv <= time rv'
+    rv >= rv' = time rv >= time rv'
+
+-- | timestamp read and write operators
+readTime :: RadioVector -> Time
+readTime rv = time rv
+
+writeTime :: RadioVector -> Time -> RadioVector
+writeTime (RadioVector _ arfcn) time = RadioVector time arfcn
+
+-- | ARFCN read and write operators
+readARFCN :: RadioVector -> Int
+readARFCN rv = arfcn rv
+
+writeARFCN :: RadioVector -> Int -> RadioVector
+writeARFCN (RadioVector time _) arfcn = RadioVector time arfcn
+
+
+
+-- | a priority queue of radioVectors, i.e. GSM bursts, sorted so that earliest element is at top
+-- class VectorQueue : public InterthreadPriorityQueue<radioVector>
+
+-- | the top element of the queue
+nextTime :: Time
+nextTime  = undefined
+
+-- | Get stale burst, if any.
+getStaleBurst :: Time              -- ^ The target time.
+              -> Maybe RadioVector -- ^ Just burst older than target time, removed from queue, or Nothing
+getStaleBurst t = undefined
+
+-- | Get current burst, if any.
+getCurrentBurst :: Time              -- ^ The target time.
+                -> Maybe RadioVector -- ^ Just burst at the target time, removed from queue, or Nothing
+getCurrentBurst t = undefined
+
+
+-- | a FIFO of RadioVectors
+
+-- XXX stub types
+data VectorFIFO = FOO
+
+--
+fifoSize :: Int
+fifoSize  = undefined
+--
+put :: RadioVector
+put  = undefined
+--
+get :: RadioVector
+get  = undefined
+
+
+-- | The basestation clock
+data RadioClock = RadioClock { rclock :: Time
+                             , signal :: Int  -- ^ XXX fix type?? pthread_cond_broadcast()
+                             }
+
+-- | Get clock value
+getClock :: RadioClock -> Time
+getClock rc = rclock rc
+
+-- | Set clock
+-- void set(const GSM::Time& wTime) { mClock = wTime; signal.broadcast(); }
+setClock :: RadioClock -> Time -> RadioClock
+setClock (RadioClock _ sig) t = RadioClock t sig
+
+-- | Increment clock
+-- void incTN() { mClock.incTN(); signal.broadcast(); }
+incClock :: RadioClock -> RadioClock
+incClock (RadioClock clk sig) = RadioClock (incTN clk 1) sig
+
+
+-- | Interface to abstract the software Transceiver with the SDR hardware backend driver
+data RadioInterface = RadioInterface { receiveFIFO      :: VectorFIFO  -- ^ FIFO that holds receive bursts
+--                                     , radio            :: RadioDevice -- ^ the SDR hw object XXX DO WE NEED THIS IN HASKELL????
+                                     , clock            :: RadioClock  -- ^ the basestation clock
+
+                                     , writeTimestamp   :: TimeStamp   -- ^ sample timestamp of next packet written to SDR hw
+                                     , readTimestamp    :: TimeStamp   -- ^ sample timestamp of next packet read from SDR hw
+
+                                     , samplesPerSymbol :: Int         -- ^ samples per GSM symbol
+                                     , receiveOffset    :: Int         -- ^ offset b/w transmit and receive GSM timestamps, in timeslots
+                                     , radioOn          :: Bool        -- ^ indicates radio is on
+                                     }
