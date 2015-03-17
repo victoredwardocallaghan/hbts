099e628a46c4f097471ec3c68704c877f9249081
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2015-03-18 07:59:44 +1100

    bts/Transceiver.hs: Attoparsec UDP ctl socket and dispatch
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- bts/Transceiver.hs
+++ bts/Transceiver.hs
@@ -1,16 +1,60 @@
-module Transceiver (controlLoop, dataLoop) where
+module Transceiver (constructTransceiver, controlLoop, dataLoop) where
 
-import Control.Monad (unless)
+import BTS.GSMTransfer
+import BTS.RadioInterface hiding (bracket)
+
 import Network.Socket hiding (recv)
 import qualified Data.ByteString.Char8 as C
 import Network.Socket.ByteString (recv, sendAll)
 import Control.Exception
 import Data.List
-import Control.Applicative ((<$>), (<*), (*>))
-import Text.Parsec
+import Control.Applicative ((<$), (<$>), (<*), (*>))
+import Control.Monad (unless, when)
+
+import Pipes
+import qualified Pipes.ByteString as P
+import qualified Data.ByteString as BS
+import Data.ByteString.Char8 (pack)
+import Data.Attoparsec.ByteString.Char8
+import Data.Foldable
 
 controlPort    = "3000"
 datastreamPort = "3001"
+
+constructTransceiver :: RadioInterface  -- ^ RadioInterface
+                     -> Int             -- ^ no. of ARFCN's
+                     -> Int             -- ^ Samples per symbol
+                     -> IO ()
+constructTransceiver ri arfcns sps = do
+  putStrLn $ "INFO " ++ "Running " ++ show arfcns ++ " ARFCN's"
+
+  -- generate pulse and setup up signal processing library
+--  gsmPulse = generateGSMPulse(2,mSamplesPerSymbol);
+--  putStrLn $ "DEBUG " ++ "gsmPulse: " ++ show gsmPulse
+--  sigProcLibSetup(mSamplesPerSymbol);
+
+--  txFullScale = mRadioInterface->fullScaleInputValue();
+--  rxFullScale = mRadioInterface->fullScaleOutputValue();
+  fsiv       <- fullScaleInputValue ri
+  fsov       <- fullScaleOutputValue ri
+
+  putStrLn "XXX to complete.."
+
+
+-- radioVector *Transceiver::fixRadioVector(BitVector &burst, int RSSI, GSM::Time &wTime, int ARFCN)
+--   -- modulate and stick into queue
+--   signalVector* modBurst = modulateBurst(burst,*gsmPulse, 8 + (wTime.TN() % 4 == 0), mSamplesPerSymbol);
+--   float headRoom = (mNumARFCNs > 1) ? 0.5 : 1.0;
+--   scaleVector(*modBurst,txFullScale * headRoom * pow(10,-RSSI/10)/mNumARFCNs);
+--   radioVector *newVec = new radioVector(*modBurst,wTime,ARFCN);
+--   return newVec;
+fixRadioVector :: BS.ByteString -- ^ burst (BitVector)
+               -> Int           -- ^ RSSI
+               -> Int           -- ^ XXX fix type Time
+               -> Int           -- ^ ARFCN
+               -> IO ()         -- ^ XXX fix type signalVector
+fixRadioVector burst rssi t arfcn = do
+  putStrLn "FIXME"
 
 -- | Drive UDP Control sock (main loop)
 controlLoop :: IO ()
@@ -54,10 +98,13 @@ procDataStream sock = do
 ctrlhandlerfunc :: (Socket, SockAddr) -> String -> IO ()
 ctrlhandlerfunc (sock, addr) msg = do
   putStrLn $ "<DEBUG> " ++ "[ " ++ show addr ++ " ]" ++ " Recieved: { " ++ msg ++ " }"
-  putStrLn $ "<DEBUG> " ++ "Sending Response: { " ++ (arfcnCmdResponseLookup (read msg) 0) ++ " }"
-  -- XXX we should dispath to the correct ARFCN command function here.
-  -- XXX should they all become actions or functions?
-  sendstr (sock,addr) $ arfcnCmdResponseLookup (read msg) 0
+  let cmd = parseOnly parseARFCNCommand (pack msg)
+  case cmd of
+    Left e  -> do putStrLn $ "ERR " ++ " badly formed data on control UDP socket"
+                  return ()
+    Right c -> do putStrLn $ "<DEBUG> " ++ "Sending Response: { " ++ arfcnCmdResponseLookup c ++ " }"
+                  sendstr (sock,addr) $ arfcnCmdResponseLookup c
+                  arfcnCmdDispatch c
   where
     -- Send until everything is done
     sendstr :: (Socket, SockAddr) -> String -> IO ()
@@ -69,14 +116,28 @@ datahandlerfunc :: (Socket, SockAddr) -> String -> IO ()
 datahandlerfunc (sock, addr) msg = do
   putStrLn $ "<DEBUG> " ++ "[ " ++ show addr ++ " ]" ++ " Recieved: { " ++ msg ++ " }"
   -- XXX finish data hot path into pipe sink
+  when (length msg == fromIntegral (gSlotLen+1+4+1)) $ putStrLn $ "ERR " ++ "badly formatted packet on GSM->TRX interface"
 
-
+  putStrLn $ "DEBUG " ++ "rcvd. burst at: " -- ++ GSM::Time(frameNum,timeSlot) <<LOGVAR(fillerFlag);
+--  int RSSI = (int) buffer[5];	// (pat) It is not RSSI, it is transmit gain, always set to 0.
+--  let rssi = 0
+--  GSM::Time currTime = GSM::Time(frameNum,timeSlot);
+--  radioVector *newVec = fixRadioVector(newBurst,RSSI,currTime,ARFCN);
+--  OK now we write newVec down into the sink
+-- ....
+--  putStrLn $ "DEBUG" ++ "added burst - time: " ++ show currTime ++ ", RSSI: " ++ show rssi ++ ", data: " ++ show newBurst
 
 -- | ..
-data ARFCNCommand = ARFCNCommandRequest | Int deriving (Show)
+data ARFCNCommand = ARFCNCommand { arfcnCmd  :: ARFCNCommandRequest
+                                 , arfcnCmdV :: Double
+                                 } deriving (Show)
 
---parseS :: String -> Either ParseError [ARFCNCommand]
---parseS input = parse content "" input
+parseARFCNCommand :: Parser ARFCNCommand
+parseARFCNCommand = do
+  c <- parseARFCNCommandRequest
+  char ' '
+  v <- double
+  return $ ARFCNCommand c v
 
 -- | ..
 data ARFCNCommandRequest = PowerOn
@@ -93,21 +154,46 @@ data ARFCNCommandRequest = PowerOn
                          | Handover
                          | NoHandover
                          | SetSlot
-                         deriving (Eq, Read)
+                         deriving (Eq, Show, Enum, Bounded)
+
+parseARFCNCommandRequest :: Parser ARFCNCommandRequest
+parseARFCNCommandRequest = asum $ map touch allCommands
+  where touch x = x <$ (string . pack . show) x
+        allCommands = [minBound .. maxBound :: ARFCNCommandRequest]
 
 -- | ..
-arfcnCmdResponseLookup :: Show a => ARFCNCommandRequest -> a -> String
-arfcnCmdResponseLookup c a | c == PowerOn       = "RSP Power 0"
+arfcnCmdResponseLookup :: ARFCNCommand -> String
+arfcnCmdResponseLookup c = arfcnCmdLookup (arfcnCmd c) (arfcnCmdV c)
+  where arfcnCmdLookup c v | c == PowerOn       = "RSP Power 0"
                            | c == PowerOff      = "RSP Power 1"
                            | c == SetRxGain     = "RSP SETRXGAIN 0"
                            | c == SetTxAtten    = "RSP SETTXATTEN 0"
-                           | c == SetFreqOffset = "RSP SETFREQOFFSET " ++ show a
-                           | c == NoiseLevel    = "RSP NOISELEV "      ++ show a
-                           | c == SetPower      = "RSP SETPOWER "      ++ show a
-                           | c == AdjustPower   = "RSP ADJUPOWER "     ++ show a
-                           | c == RxTune        = "RSP RXTUNE "        ++ show a
-                           | c == TxTune        = "RSP TXTUNE "        ++ show a
-                           | c == SetTsc        = "RSP SETTSC "        ++ show a
-                           | c == Handover      = "RSP HANDOVER "      ++ show a
-                           | c == NoHandover    = "RSP NOHANDOVER "    ++ show a
-                           | c == SetSlot       = "RSP SETSLOT "       ++ show a
+                           | c == SetFreqOffset = "RSP SETFREQOFFSET " ++ show v
+                           | c == NoiseLevel    = "RSP NOISELEV "      ++ show v
+                           | c == SetPower      = "RSP SETPOWER "      ++ show v
+                           | c == AdjustPower   = "RSP ADJUPOWER "     ++ show v
+                           | c == RxTune        = "RSP RXTUNE "        ++ show v
+                           | c == TxTune        = "RSP TXTUNE "        ++ show v
+                           | c == SetTsc        = "RSP SETTSC "        ++ show v
+                           | c == Handover      = "RSP HANDOVER "      ++ show v
+                           | c == NoHandover    = "RSP NOHANDOVER "    ++ show v
+                           | c == SetSlot       = "RSP SETSLOT "       ++ show v
+
+
+-- | XXX figure out which actions to dispatch to?
+arfcnCmdDispatch :: ARFCNCommand -> IO ()
+arfcnCmdDispatch c = arfcnCmdDispatcher (arfcnCmd c) (arfcnCmdV c)
+  where arfcnCmdDispatcher c v | c == PowerOn       = putStrLn ".."
+                               | c == PowerOff      = putStrLn ".."
+                               | c == SetRxGain     = putStrLn ".."
+                               | c == SetTxAtten    = putStrLn ".."
+                               | c == SetFreqOffset = putStrLn ".."
+                               | c == NoiseLevel    = putStrLn ".."
+                               | c == SetPower      = putStrLn ".."
+                               | c == AdjustPower   = putStrLn ".."
+                               | c == RxTune        = putStrLn ".."
+                               | c == TxTune        = putStrLn ".."
+                               | c == SetTsc        = putStrLn ".."
+                               | c == Handover      = putStrLn ".."
+                               | c == NoHandover    = putStrLn ".."
+                               | c == SetSlot       = putStrLn ".."
